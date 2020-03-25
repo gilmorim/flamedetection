88e01c5fe3b60dda815f3c8b9d98394c3d150768
Gilberto Morim <a65214@alunos.uminho.pt>
2020-03-25 00:59:04 +0000

    added readme

---
+++ README.md
@@ -0,0 +1,3 @@
+#Fire alarm
+
+![Flame Detector Schematic](https://github.com/gilmorim/flamedetection/blob/master/arduino/img/flamedetect_schematic.png?raw=true)
--- arduino/flamedetect.ino
+++ arduino/flamedetect.ino
@@ -1,24 +1,39 @@
+/**
+ * 
+ * @file	flamedetect.ino
+ * @github	https://github.com/gilmorim
+ * @author	Gilberto Morim <gilgmorim@gmail.com>
+ * @date	2020-03-25
+ * @brief	Arduino code to receive signal from flame detection sensor, and activate buzzer when it does so.
+ * 
+ */
+
 #include <SoftwareSerial.h>
 
 #define PIN_FLAME_SENSOR 2
 #define PIN_BUZZER 3
 
-void setup(){
+void setup()
+{
   Serial.begin(9600);
   Serial.println("Starting test...");
   pinMode(PIN_FLAME_SENSOR, INPUT);
   pinMode(PIN_BUZZER, OUTPUT);
 }
 
-void loop(){
+void loop()
+{
   int fire = digitalRead(FLAME);
 
-  if(fire == HIGH){
+  if (fire == HIGH)
+  {
     digitalWrite(BUZZ, HIGH);
-    // Serial.println("Fire!");  
-  } else {
+    // Serial.println("Fire!");
+  }
+  else
+  {
     digitalWrite(BUZZ, LOW);
-    // Serial.println("Peace...");  
+    // Serial.println("Peace...");
   }
 
   delay(200);
